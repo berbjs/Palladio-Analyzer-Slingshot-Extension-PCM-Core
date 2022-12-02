f1cb8ba0c59a5398e8fd8b6bc4f158febcd197c5
Floriment Klinaku <klinakuf@gmail.com>
2022-12-02 13:46:00 +0100

    applying commit 3b6e05959855dc97f9562c39af68170721bf47d4

--- org.palladiosimulator.analyzer.slingshot.behavior.resourcesimulation.monitor/src/org/palladiosimulator/analyzer/slingshot/behavior/resourcesimulation/monitor/PassiveResourceProbeTable.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.resourcesimulation.monitor/src/org/palladiosimulator/analyzer/slingshot/behavior/resourcesimulation/monitor/PassiveResourceProbeTable.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.palladiosimulator.analyzer.slingshot.behavior.systemsimulation.events.PassiveResourceAcquired;
 import org.palladiosimulator.analyzer.slingshot.behavior.systemsimulation.events.PassiveResourceReleased;
 import org.palladiosimulator.analyzer.slingshot.behavior.systemsimulation.events.ResourceDemandRequested;
+import org.palladiosimulator.analyzer.slingshot.common.events.DESEvent;
+import org.palladiosimulator.analyzer.slingshot.monitor.probes.EventDistinguisher;
 import org.palladiosimulator.analyzer.slingshot.monitor.utils.probes.EventCurrentSimulationTimeProbe;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.pcm.repository.PassiveResource;
@@ -13,8 +15,14 @@ import org.palladiosimulator.pcmmeasuringpoint.AssemblyPassiveResourceMeasuringP
 import org.palladiosimulator.probeframework.calculator.Calculator;
 import org.palladiosimulator.probeframework.calculator.DefaultCalculatorProbeSets;
 import org.palladiosimulator.probeframework.calculator.IGenericCalculatorFactory;
+import org.palladiosimulator.probeframework.measurement.RequestContext;
 import org.palladiosimulator.probeframework.probes.Probe;
 
+
+/**
+ * @author Julijan Katic, Floriment Klinaku, Sarah Stiess
+ *
+ */
 public final class PassiveResourceProbeTable {
 
 	private final Map<String, Probes> probes = new HashMap<>();
@@ -58,25 +66,59 @@ public final class PassiveResourceProbeTable {
 			final IGenericCalculatorFactory calculatorFactory) {
 		this.addPassiveResource(measuringPoint.getPassiveResource());
 		final Probes probes = this.probes.get(measuringPoint.getPassiveResource().getId());
-		return calculatorFactory.buildCalculator(MetricDescriptionConstants.WAITING_TIME_METRIC, measuringPoint,
+		return calculatorFactory.buildCalculator(MetricDescriptionConstants.WAITING_TIME_METRIC_TUPLE, measuringPoint,
 				DefaultCalculatorProbeSets.createStartStopProbeConfiguration(probes.resourceDemandRequestedProbe,
 						probes.passiveResourceAcquiredProbe));
 	}
-
-	private static final class Probes {
-		private final EventCurrentSimulationTimeProbe resourceDemandRequestedProbe = new EventCurrentSimulationTimeProbe();
-		private final EventCurrentSimulationTimeProbe passiveResourceAcquiredProbe = new EventCurrentSimulationTimeProbe();
-		private final EventCurrentSimulationTimeProbe passiveResourceReleasedProbe = new EventCurrentSimulationTimeProbe();
-	}
-
+	
 	public Calculator setupHoldingTimeCalculator(final AssemblyPassiveResourceMeasuringPoint measuringPoint,
 			final IGenericCalculatorFactory calculatorFactory) {
 		this.addPassiveResource(measuringPoint.getPassiveResource());
 		final Probes probes = this.probes.get(measuringPoint.getPassiveResource().getId());
-		return calculatorFactory.buildCalculator(MetricDescriptionConstants.HOLDING_TIME_METRIC, measuringPoint,
+		return calculatorFactory.buildCalculator(MetricDescriptionConstants.HOLDING_TIME_METRIC_TUPLE, measuringPoint,
 				DefaultCalculatorProbeSets.createStartStopProbeConfiguration(probes.passiveResourceAcquiredProbe,
 						probes.passiveResourceReleasedProbe));
 	}
+
+	private static final class Probes {
+		private final EventCurrentSimulationTimeProbe resourceDemandRequestedProbe = new EventCurrentSimulationTimeProbe(
+				new ResourceDemandRequestEventDistinguisher());
+		private final EventCurrentSimulationTimeProbe passiveResourceAcquiredProbe = new EventCurrentSimulationTimeProbe(new PassiveResourceAcquiredEventDistinuisher());
+		private final EventCurrentSimulationTimeProbe passiveResourceReleasedProbe = new EventCurrentSimulationTimeProbe(new PassiveResourceResleasedEventDistinuisher());
+
+		private static final class ResourceDemandRequestEventDistinguisher implements EventDistinguisher {
+
+			@Override
+			public RequestContext apply(DESEvent t) {
+				return new RequestContext(((ResourceDemandRequested) t).getEntity().getSeffInterpretationContext()
+						.getRequestProcessingContext().getUserId());
+			}
+
+		}
+
+		private static final class PassiveResourceAcquiredEventDistinuisher implements EventDistinguisher {
+
+			@Override
+			public RequestContext apply(DESEvent t) {
+				return new RequestContext(((PassiveResourceAcquired) t).getEntity().getSeffInterpretationContext()
+						.getRequestProcessingContext().getUserId());
+			}
+
+		}
+		private static final class PassiveResourceResleasedEventDistinuisher implements EventDistinguisher {
+			
+			@Override
+			public RequestContext apply(DESEvent t) {
+				return new RequestContext(((PassiveResourceReleased) t).getEntity().getSeffInterpretationContext()
+						.getRequestProcessingContext().getUserId());
+			}
+			
+		}
+
+	}
+
+
+
 
 
 }
