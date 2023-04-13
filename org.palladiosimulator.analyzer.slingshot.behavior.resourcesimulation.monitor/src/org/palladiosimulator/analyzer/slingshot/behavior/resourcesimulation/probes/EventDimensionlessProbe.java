e13dcf4105b139fa8cf7ce4babd753397ccb9936
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-04-13 18:08:14 +0200

    use list base probe.

--- org.palladiosimulator.analyzer.slingshot.behavior.resourcesimulation.monitor/src/org/palladiosimulator/analyzer/slingshot/behavior/resourcesimulation/probes/EventDimensionlessProbe.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.resourcesimulation.monitor/src/org/palladiosimulator/analyzer/slingshot/behavior/resourcesimulation/probes/EventDimensionlessProbe.java
@@ -1,31 +1,19 @@
 package org.palladiosimulator.analyzer.slingshot.behavior.resourcesimulation.probes;
 
-import static org.palladiosimulator.metricspec.constants.MetricDescriptionConstants.POINT_IN_TIME_METRIC;
-
-import javax.measure.quantity.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.measure.Measure;
 import javax.measure.quantity.Dimensionless;
-import javax.measure.unit.SI;
-
 import org.palladiosimulator.analyzer.slingshot.behavior.resourcesimulation.events.ActiveResourceStateUpdated;
 import org.palladiosimulator.analyzer.slingshot.common.events.DESEvent;
-import org.palladiosimulator.analyzer.slingshot.monitor.probes.EventBasedProbe;
+import org.palladiosimulator.analyzer.slingshot.monitor.probes.EventBasedListProbe;
 import org.palladiosimulator.analyzer.slingshot.monitor.probes.EventDistinguisher;
-import org.palladiosimulator.measurementframework.BasicMeasurement;
-import org.palladiosimulator.measurementframework.MeasuringValue;
-import org.palladiosimulator.measurementframework.measureprovider.MeasurementListMeasureProvider;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
-import org.palladiosimulator.probeframework.measurement.ProbeMeasurement;
 
 /**
  *
  * @author stiesssh
  *
  */
-public final class EventDimensionlessProbe extends EventBasedProbe<Long, Dimensionless> {
+public final class EventDimensionlessProbe extends EventBasedListProbe<Long, Dimensionless> {
 
 	/**
 	 *
@@ -54,31 +42,27 @@ public final class EventDimensionlessProbe extends EventBasedProbe<Long, Dimensi
 		throw new IllegalArgumentException("event not of type ACTIVERESOUCRESTATEUPDATED");
 	}
 
-	public Measure<Double, Duration> getTime(final DESEvent event) {
-		return Measure.valueOf(event.time(), SI.SECOND);
-	}
-
-	@Override
-	protected ProbeMeasurement getProbeMeasurement(final DESEvent event) {
-		final List<MeasuringValue> list = new ArrayList<>(2);
-
-		final BasicMeasurement<Double, Duration> timeMeasurement = new BasicMeasurement<>(
-				this.getTime(event), MetricDescriptionConstants.POINT_IN_TIME_METRIC);
-
-        final MeasuringValue pointInTimeMeasurement = timeMeasurement.getMeasuringValueForMetric(POINT_IN_TIME_METRIC);
-		list.add(pointInTimeMeasurement);/// time
-
-
-		final BasicMeasurement<Long, Dimensionless> valueMeasurement = new BasicMeasurement<>(
-				this.getMeasurement(event), MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC);
-
-        final MeasuringValue valueFooMeasurement = valueMeasurement.getMeasuringValueForMetric(MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC);
-		list.add(valueFooMeasurement);/// value
-
-
-		final MeasurementListMeasureProvider resultMeasurement = new MeasurementListMeasureProvider(list);
-
-		return new ProbeMeasurement(resultMeasurement, this, this.getDistinguisher().apply(event));
-	}
+//	@Override
+//	protected ProbeMeasurement getProbeMeasurement(final DESEvent event) {
+//		final List<MeasuringValue> list = new ArrayList<>(2);
+//
+//		final BasicMeasurement<Double, Duration> timeMeasurement = new BasicMeasurement<>(
+//				this.getTime(event), MetricDescriptionConstants.POINT_IN_TIME_METRIC);
+//
+//        final MeasuringValue pointInTimeMeasurement = timeMeasurement.getMeasuringValueForMetric(POINT_IN_TIME_METRIC);
+//		list.add(pointInTimeMeasurement);/// time
+//
+//
+//		final BasicMeasurement<Long, Dimensionless> valueMeasurement = new BasicMeasurement<>(
+//				this.getMeasurement(event), MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC);
+//
+//        final MeasuringValue valueFooMeasurement = valueMeasurement.getMeasuringValueForMetric(MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC);
+//		list.add(valueFooMeasurement);/// value
+//
+//
+//		final MeasurementListMeasureProvider resultMeasurement = new MeasurementListMeasureProvider(list);
+//
+//		return new ProbeMeasurement(resultMeasurement, this, this.getDistinguisher().apply(event));
+//	}
 
 }
