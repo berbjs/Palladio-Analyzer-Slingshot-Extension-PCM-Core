4f66b29c90b1d331f8358d5a19e0350d81bc6ca5
Floriment Klinaku <klinakuf@gmail.com>
2024-02-08 19:05:58 +0100

    added missing events"

---
+++ org.palladiosimulator.analyzer.slingshot.behavior.systemsimulation.data/src/org/palladiosimulator/analyzer/slingshot/behavior/systemsimulation/events/ResourceDemandRequestAborted.java
@@ -0,0 +1,12 @@
+package org.palladiosimulator.analyzer.slingshot.behavior.systemsimulation.events;
+
+import org.palladiosimulator.analyzer.slingshot.behavior.systemsimulation.entities.resource.ResourceDemandRequest;
+
+public class ResourceDemandRequestAborted extends AbstractResourceRequestEvent {
+
+	public ResourceDemandRequestAborted(ResourceDemandRequest entity) {
+		super(entity, 0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
